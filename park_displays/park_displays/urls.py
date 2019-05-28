fd4eb710687bacfca30faa4bbff4476e1f43f276
Marcello Feroce <marcello.feroce@tum.de>
2019-05-28 14:13:14 +0200

    Modified project name. Created park_displays_app. Created simple index view

--- park_displays/park_displays/urls.py
+++ park_displays/park_displays/urls.py
@@ -1,4 +1,4 @@
-"""park_displays_app URL Configuration
+"""park_displays URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include,path
 
 urlpatterns = [
+    path('park_displays_app/', include('park_displays_app.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ park_displays/park_displays_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ParkDisplaysAppConfig(AppConfig):
+    name = 'park_displays_app'

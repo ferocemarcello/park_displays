20a18a0d9f3e69268b997c62c9c6d29a5d3ba578
Marcello Feroce <marcello.feroce@tum.de>
2019-06-05 17:47:26 +0200

    added static root variable in settings.py

--- park_displays/park_displays/settings.py
+++ park_displays/park_displays/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT = "/home/parkdisplays/park_displays/park_displays/park_displays_app/static"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/

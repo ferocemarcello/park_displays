ed8a1f1f2cc1b41dc0d3e9806dd74890f56c184f
Marcello Feroce <marcello.feroce@tum.de>
2019-06-05 17:26:31 +0200

    Added localhost and pythonanywhere to allowed hosts

--- park_displays/park_displays/settings.py
+++ park_displays/park_displays/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tg=(=4@=$)-tz%p0&)mh%!&1khmo-av3$mb&!d+72e95y*u*v_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['parkdisplays.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition

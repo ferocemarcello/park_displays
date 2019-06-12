de7491ea8368f1b610f112ab8f5d8a841c9c2a47
Marcello Feroce <marcello.feroce@tum.de>
2019-06-12 15:16:48 +0200

    added recommendation for outdoor gym

--- park_displays/park_displays_app/recommendation.py
+++ park_displays/park_displays_app/recommendation.py
@@ -9,3 +9,13 @@ class RunWalkRecommender():
         self.activity=activity
     def recommendPaths(self):
         return []
+class GymRecommender():
+    def __init__(self,gender,age,weight,height,kcal,bodyparts:[]):
+        self.gender=gender
+        self.age=age
+        self.weight=weight
+        self.height=height
+        self.kcal=kcal
+        self.bodyparts=bodyparts
+    def recommendPaths(self):
+        return [(None,None,None)]#list of (machine, time, intensity)   intensity can be speed, weight, Force...

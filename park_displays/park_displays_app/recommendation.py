d547c3a22067823168033d684ad5302f156feb39
Marcello Feroce <marcello.feroce@tum.de>
2019-06-12 15:35:43 +0200

    added recommendation for groups

--- park_displays/park_displays_app/recommendation.py
+++ park_displays/park_displays_app/recommendation.py
@@ -20,7 +20,7 @@ class GymRecommender():
     def recommendActivities(self):
         return [(None,None,None)]#list of (machine, time, intensity)   intensity can be speed, weight, Force...
 class FreeweightStretchingRecommender():
-    def __init__(self,gender,age,weight,height,kcal,intensity):
+    def __init__(self,gender,age,weight,height,kcal,intensity, stretching=False, freeweight=False):
         self.gender=gender
         self.age=age
         self.weight=weight
@@ -28,4 +28,10 @@ class FreeweightStretchingRecommender():
         self.kcal=kcal
         self.intensity=intensity
     def recommendExercises(self):
-        return [(None,None)]#list of (exercise, time)
+        return [(None,None,None)]#list of (exercise, time, breaktime)
+class GroupRecommender():
+    def __init__(self,group,intensity,stretching=False, freeweight=False):
+        self.group=group
+        self.intensity=intensity
+    def recommendExercises(self):
+        return [(None,None,None)]#list of (exercise, time, breaktime)

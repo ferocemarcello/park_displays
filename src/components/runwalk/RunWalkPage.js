8859274b820606d810c6b5c2b2debed4706cf406
Thomas Raith <thomas.raith@tum.de>
2019-06-19 14:02:02 +0200

    Add Components for List Item (RunWalkTrack)

--- src/components/runwalk/RunWalkPage.js
+++ src/components/runwalk/RunWalkPage.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react';
 import { Map, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './RunWalkPage.module.scss';
 import data from './data.json';
+
+class TrackListItem extends Component {
+  render() {
+    const { trackId, trackName, trackDistance, trackDuration, trackUp, trackDown, trackGroundType, trackAnnotation, waypoints } = this.props;
+    return (
+      <Link to={`/runwalk/track/${trackId}`} className={styles['ListItem']}>
+        <Map bounds={waypoints} zoom={15} style={{height: 220, width: 440}} zoomControl={false} doubleClickZoom={false} boxZoom={false} dragging={false} keyboard={false} scrollWheelZoom={false} touchZoom={false}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
+          <Marker position={[48.161126,11.598448]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+          <Polyline color="lime" positions={waypoints} />
+        </Map>
+        <div className={styles['ListItemTextSection']}>
+          <div className={styles['ListItemTextSectionTitle']}>{trackName}</div>
+          <div className={styles['ListItemTextSectionBody']}>
+            <p>
+              <FontAwesomeIcon icon="arrows-alt-h" /> {trackDistance}&nbsp;&nbsp;&nbsp;
+              <FontAwesomeIcon icon="clock" /> {trackDuration}&nbsp;&nbsp;&nbsp;
+              <FontAwesomeIcon icon="arrow-up" /> {trackUp}&nbsp;&nbsp;&nbsp;
+              <FontAwesomeIcon icon="arrow-down" /> {trackDown}&nbsp;&nbsp;&nbsp;
+              <FontAwesomeIcon icon="layer-group" /> {trackGroundType}
+            </p>
+            <p className={styles['ListItemTextSectionDescription']}>
+              {trackAnnotation}
+            </p>
+          </div>
+        </div>
+      </Link>
+    );
+  }
+}
 
 class RunWalkPage extends Component {
 
@@ -50,47 +87,60 @@ class RunWalkPage extends Component {
           </div>
         </div>
         <div className={styles['ListSection']}>
-          <div className={styles['ListItem']}>
-            <Map bounds={data.waypoints} zoom={15} style={{height: 220, width: 440}} zoomControl={false} doubleClickZoom={false} boxZoom={false} dragging={false} keyboard={false} scrollWheelZoom={false} touchZoom={false}>
-              <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
-              <Marker position={[48.161126,11.598448]}>
-                <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-              </Marker>
-              { this.state.line ? <Polyline color="lime" positions={data.waypoints} /> : null }
-            </Map>
-            <div className={styles['ListItemTextSection']}>
-              <div style={{fontSize: '1.5rem', paddingTop: 16}}>Englischer Garten 1</div>
-              <div style={{color: 'grey'}}>
-                <p>
-                <FontAwesomeIcon icon="arrows-alt-h" /> 5,4 km&nbsp;&nbsp;&nbsp;
-                <FontAwesomeIcon icon="clock" /> 1:30 h&nbsp;&nbsp;&nbsp;
-                <FontAwesomeIcon icon="arrow-up" /> 4 hm&nbsp;&nbsp;&nbsp;
-                <FontAwesomeIcon icon="arrow-down" /> 12 hm&nbsp;&nbsp;&nbsp;
-                  <FontAwesomeIcon icon="layer-group" /> Gravel
-                </p>
-                <p style={{fontFamily: 'Helvetica'}}>
-                  Hier könnte jetzt noch irgendein Text stehen, der den Nutzer einlädt die Strecke zu laufen.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className={styles['MapSection']} id="map-section">
-          <Map center={centerPosition} zoom={15} style={{height: '100%'}}>
-            <TileLayer
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
-            <Marker position={centerPosition}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
-            { this.state.line ? <Polyline color="lime" positions={data.waypoints} /> : null }
-          </Map>
+          <TrackListItem
+            trackId="00001"
+            trackName="Englischer Garten 1"
+            trackDistance="5.4 km"
+            trackDuration="1:30 h"
+            trackUp="4 hm"
+            trackDown="12 hm"
+            trackGroundType="Gravel"
+            trackAnnotation="Hier könnte jetzt noch irgendein Text stehen, der den Nutzer einlädt die Strecke zu laufen."
+            waypoints={data.waypoints} />
+
+          <TrackListItem
+            trackId="00001"
+            trackName="Englischer Garten 2"
+            trackDistance="5.4 km"
+            trackDuration="1:30 h"
+            trackUp="4 hm"
+            trackDown="12 hm"
+            trackGroundType="Gravel"
+            trackAnnotation="Hier könnte jetzt noch irgendein Text stehen, der den Nutzer einlädt die Strecke zu laufen."
+            waypoints={data.waypoints} />
+
+          <TrackListItem
+            trackId="00001"
+            trackName="Englischer Garten 3"
+            trackDistance="5.4 km"
+            trackDuration="1:30 h"
+            trackUp="4 hm"
+            trackDown="12 hm"
+            trackGroundType="Gravel"
+            trackAnnotation="Hier könnte jetzt noch irgendein Text stehen, der den Nutzer einlädt die Strecke zu laufen."
+            waypoints={data.waypoints} />
+
+          <TrackListItem
+            trackId="00001"
+            trackName="Englischer Garten 4"
+            trackDistance="5.4 km"
+            trackDuration="1:30 h"
+            trackUp="4 hm"
+            trackDown="12 hm"
+            trackGroundType="Gravel"
+            trackAnnotation="Hier könnte jetzt noch irgendein Text stehen, der den Nutzer einlädt die Strecke zu laufen."
+            waypoints={data.waypoints} />
+
+          <TrackListItem
+            trackId="00001"
+            trackName="Englischer Garten 5"
+            trackDistance="5.4 km"
+            trackDuration="1:30 h"
+            trackUp="4 hm"
+            trackDown="12 hm"
+            trackGroundType="Gravel"
+            trackAnnotation="Hier könnte jetzt noch irgendein Text stehen, der den Nutzer einlädt die Strecke zu laufen."
+            waypoints={data.waypoints} />
         </div>
       </div>
     )

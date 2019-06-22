fae72f1ce480609d624c31ecd8e3163ea7b3ff01
Marcello Feroce <marcello.feroce@tum.de>
2019-06-22 13:49:30 +0200

    refactored and optimized gym and runwalk recommendation

--- park_displays/park_displays_app/recommendation.py
+++ park_displays/park_displays_app/recommendation.py
@@ -5,7 +5,7 @@ from .xmlmanager import XmlManager
 from .weather import Weather
 from datetime import  datetime
 import pytz
-APPID="676536c0f3533a7868a16beed9f14ba4"
+WEATHERAPPID= "676536c0f3533a7868a16beed9f14ba4"
 kelvin_zero_celsius=273.15
 #https://openweathermap.org/weather-conditions
 weatherdescthunderstorm={200:"thunderstorm with light rain",201:"thunderstorm with rain",202:"thunderstorm with heavy rain",210:"light thunderstorm",211:"thunderstorm",212:"heavy thunderstorm",221:"ragged thunderstorm",230:"thunderstorm with light drizzle",231:"thunderstorm with drizzle",232:"thunderstorm with heavy drizzle"}
@@ -21,43 +21,43 @@ def getScoreOnBody(score, gender,age,weight,height,avgweekkm=None):
     # score based on gender, age, bmi, avgweekkm
     if gender.lower() == "female":
         score -= 10
-    if gender.lower() == "other":
+    elif gender.lower() == "other":
         score -= 5
     if age <= 14:
         score -= 20
-    if age >= 15 and age <= 18:
+    elif 15<=age <= 18:
         score -= 10
-    if age >= 19 and age <= 23:
+    elif 19<=age <= 23:
         score -= 5
-    if age >= 24 and age <= 33:
-        pass
-    if age >= 34 and age <= 42:
+    elif 24<=age <= 33:
+        pass#perfect age for physical activity
+    elif 34<=age <= 42:
         score -= -10
-    if age >= 43 and age <= 55:
+    elif 43<=age<= 55:
         score -= 20
-    if age >= 56 and age <= 67:
+    elif 56<= age <= 67:
         score -= 25
-    if age >= 68 and age >= 75:
+    elif 68<= age<=75:
         score -= 30
-    if age >= 76:
+    elif age >= 76:
         score -= 35
     bmi = (weight) / (height ^ 2)
     if bmi < 16:
         score -= 10
-    if bmi < 18.5 and bmi >= 16:
+    elif 16<= bmi < 18.5:
         score -= 5
-    if bmi >= 18.5 and bmi < 25:
-        pass
-    if bmi >= 25 and bmi < 30:
+    elif 18.5<=bmi < 25:
+        pass#perfect bmi
+    elif 25<=bmi < 30:
         score -= 5
-    if bmi >= 30 and bmi < 35:
+    elif 30<= bmi < 35:
         score -= 10
-    if bmi >= 35:
+    elif bmi >= 35:
         score -= 15
     if avgweekkm!=None:
         if avgweekkm < 30:
             score -= 10
-        if avgweekkm >= 30 and avgweekkm < 60:
+        elif 30<=avgweekkm < 60:
             score -= 5
     return score
 class RunWalkRecommender():
@@ -74,17 +74,16 @@ class RunWalkRecommender():
         self.shoetype=shoetype
     def recommendPaths(self):
         tz = pytz.timezone('Europe/Berlin')
-        berlin_now = datetime.now(tz)
-        hour = berlin_now.hour
-        minute = berlin_now.minute
-        weather=Weather("Munich",APPID)
+        #berlin_now = datetime.now(tz)
+        #hour = berlin_now.hour
+        #minute = berlin_now.minute
+        weather=Weather("Munich", WEATHERAPPID)
         if self.activity=='running':
             return self.recommendRunning(weather)
         else:
             return self.recommendWalking(weather)
     def recommendRunning(self,weather):
-        score=100
-        score=getScoreOnBody(score,self.gender,self.age,self.weight,self.height,self.avgweekkm)# worst score depending on body only is 100-70=30
+        score=getScoreOnBody(score=100,gender=self.gender,age=self.age,weight=self.weight,height=self.height,avgweekkm=self.avgweekkm)# worst score depending on body only is 100-70=30
         score=self.getScoreOnWeather(score,weather)# worst score depending on weather only is 100-25=75
         # worst score depending on body and weather is 100-70-25=5
         pathscores=self.getPathScores(score,"running")#shoetype depending on weight and path type, and length, steepness, desired kcal
@@ -109,19 +108,19 @@ class RunWalkRecommender():
             score-=5
         if weather.temp<5+kelvin_zero_celsius or weather.temp>25+kelvin_zero_celsius:
             score -= 2
-        if weather.temp<0+kelvin_zero_celsius or weather.temp>30+kelvin_zero_celsius:
+        elif weather.temp<0+kelvin_zero_celsius or weather.temp>30+kelvin_zero_celsius:
             score -= 3
-        if weather.hum>80 and weather.hum<=90:
+        if 80<weather.hum<=90:
             score -= 2
-        if weather.hum>90:
+        elif weather.hum>90:
             score-=3
         if weather.wind_speed>=10:
             score-=2
-        if weather.wind_speed>=17:
+        elif weather.wind_speed>=17:
             score-=3
         if weather.pres<=920:
             score-=2
-        if weather.pres<=870:
+        elif weather.pres<=870:
             score-=3
         return score
 
@@ -129,26 +128,26 @@ class RunWalkRecommender():
         # shoetype depending on weight and path type, and length, steepness, desired kcal
         weightperkm=self.weight
         if atleteCondition>90:
-            weightperkm=self.weight-0.2*self.weight
-        if atleteCondition>80 and atleteCondition<=90:
-            weightperkm = self.weight - 0.1 * self.weight
-        if atleteCondition<60:
-            weightperkm = self.weight + 0.2 * self.weight
-        if atleteCondition<70 and atleteCondition>=60:
-            weightperkm = self.weight + 0.1 * self.weight
-        pathscores = [100] * len(self.paths)
+            weightperkm-=0.2*self.weight
+        elif 80<atleteCondition<=90:
+            weightperkm -=0.1 * self.weight
+        elif atleteCondition<60:
+            weightperkm +=0.2 * self.weight
+        elif 60<=atleteCondition<70:
+            weightperkm+= 0.1 * self.weight
+        pathscores = [100] * len(self.paths)#every score path is up to 100
         for i in range(len(self.paths)):
             if int(self.paths[i][0][2])!=0:#steepness different than 0
                 weightperkm+=weightperkm*(int(self.paths[i][0][2])/100)#eg at steepness level weight=70, steepness 2% =>weight=70+70*(2/100)=70+70*0.02=70+1.4=71.4
             if self.paths[i][0][1]=="Pavement":
                 weightperkm -= 0.01 * weightperkm
-            if self.paths[i][0][1]=="Dirt":
+            elif self.paths[i][0][1]=="Dirt":
                 weightperkm += 0.01 * weightperkm
             if self.shoetype in ["Road A3","Trail"]:
                 weightperkm+=0.01*weightperkm
-            if self.shoetype in ["Road A1","Track"]:
+            elif self.shoetype in ["Road A1","Track"]:
                 weightperkm-=0.01*weightperkm
-            if activity=="walking":
+            if activity.lower()=="walking":
                 weightperkm*=0.40
             requiredkcal=weightperkm*(int(self.paths[i][0][3]))/1000#adjusted weight*path length in km
             pathscores[i] =(1-(abs(self.kcal - requiredkcal) / self.kcal))*100#(1-relative error)*100
@@ -165,8 +164,7 @@ class GymRecommender():
         self.machines=machines
         self.gymtool_scores=gymtool_scores
     def recommendActivities(self):
-        bodyscore=100
-        bodyscore=getScoreOnBody(bodyscore,self.gender,self.age,self.weight,self.height)
+        bodyscore=getScoreOnBody(score=100,gender=self.gender,age=self.age,weight=self.weight,height=self.height)
         machinescores=self.getMachinesWithScores()
         machines_with_time=self.getMachinesWithTime(machinescores,bodyscore)
         return machines_with_time#list of (machine, time)
@@ -188,22 +186,9 @@ class GymRecommender():
         return machinescores
 
     def getMachinesWithTime(self, machinescores, bodyscore):
-        divider=7
-        if self.weight<60:
-            divider=7.5
-        if self.weight>=60 and divider<75:
-            divider=7
-        if self.weight>=75:
-            divider=6.5
-        if bodyscore>90:
-            divider+=0.2
-        if bodyscore>80 and bodyscore<=90:
-            divider += 0.3
-        if bodyscore<60:
-            divider -= 0.3
-        if bodyscore<70 and bodyscore>=60:
-            divider -= 0.2
-        time=self.kcal/divider#minutes
+        kcalminute=self.getKcalMin()
+        kcalminute=self.adjustKcalMinBodyScore(bodyscore=bodyscore,kcalminute=kcalminute)
+        time=self.kcal/kcalminute#minutes
         machinescoressum = {}
         for machinescore in machinescores:
             sumscoremachine=0
@@ -211,9 +196,7 @@ class GymRecommender():
                 sumscoremachine += int(machinescore[1][bodypart])
             machinescoressum[machinescore[0]]=sumscoremachine
         ratiosmachines={}
-        totsumscore=0
-        for machine in machinescoressum.keys():
-            totsumscore+=machinescoressum[machine]
+        totsumscore=sum([machinescoressum[key] for key in machinescoressum.keys()])
         for machine in machinescoressum.keys():
             ratiosmachines[machine]=machinescoressum[machine]/totsumscore
         machineswithtime={}
@@ -222,6 +205,22 @@ class GymRecommender():
 
         return machineswithtime
 
+    def getKcalMin(self):#light people burn less kcal per minute
+        if self.weight<60:
+            return  6.5
+        elif 60<=self.weight<75:
+            return 7
+        return 7.5
+
+    def adjustKcalMinBodyScore(self, bodyscore, kcalminute):
+        if bodyscore>90:
+            return  + 0.2
+        elif 80<bodyscore<=90:
+            return kcalminute + 0.3
+        elif bodyscore<60:
+            return kcalminute - 0.3
+        elif 60<=bodyscore<70:
+            return kcalminute -0.2
 
 class FreeweightStretchingRecommender():
     def __init__(self,gender,age,weight,height,kcal,intensity, stretching=False, freeweight=False):

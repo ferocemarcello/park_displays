8b0afe868757661b6c28d2e093a40cdbdf6990cb
Thomas Raith <thomas@raith.pro>
2019-06-25 18:06:43 +0200

    Add Parkmap (without routes, landmarks ect.)

--- src/components/ParkMapPage/ParkMapPage.js
+++ src/components/ParkMapPage/ParkMapPage.js
@@ -6,8 +6,7 @@ class ParkMapPage extends Component {
   render() {
     return (
       <div className={styles['ParkMapPage']}>
-        ParkMapPage
-        <Map center={[48.112824, 11.6295388]} zoom={15} style={{height: 400}}>
+        <Map center={[48.1642323, 11.6033635]} zoom={14} style={{height: 'calc(100vh - 96px)'}}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
           <Marker position={[48.112824, 11.6295388]}>
             <Popup>

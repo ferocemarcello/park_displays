227e5d83a4cec01b4913f5ec918696bfd82bcf89
Thomas Raith <thomas@raith.pro>
2019-07-01 11:02:47 +0200

    Enhance Park Map

--- src/components/ParkMapPage/ParkMapPage.js
+++ src/components/ParkMapPage/ParkMapPage.js
@@ -12,15 +12,32 @@ class ParkMapPage extends Component {
     iconSize: [32, 32]
   });
 
+  leafletWaterIcon = icon({
+    iconUrl: 'water.png',
+    iconSize: [32, 32]
+  });
+
+  leafletGymtoolIcon = icon({
+    iconUrl: 'gymtool.png',
+    iconSize: [32, 32]
+  });
+
 
   render() {
+
     return (
       <div className={styles['ParkMapPage']}>
         <Map center={[48.1642323, 11.6033635]} zoom={14} style={{height: 'calc(100vh - 96px)'}}>
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
           {
             Landmarks.fountains.map((landmark, index) =>
-              <Marker position={[landmark.lat, landmark.lon]} opacity="0.9" icon={this.leafletMonumentIcon} key={index}>
+              <Marker position={landmark.location} opacity="0.9" icon={this.leafletWaterIcon} key={index} />
+            )
+          }
+
+          {
+            Landmarks.gymtools.map((landmark, index) =>
+              <Marker position={landmark.location} opacity="0.9" icon={this.leafletGymtoolIcon} key={index}>
                 <Popup>
                   {landmark.name.replace('\\', '').replace('\\', '')}
                 </Popup>

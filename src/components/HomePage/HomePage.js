f260273e2c586ad74027a21fe26044a0ead24c32
Thomas Raith <thomas@raith.pro>
2019-07-03 08:22:48 +0200

    Remove Sliders from Home Page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -27,7 +27,7 @@ class SliderItem extends Component {
   render() {
     const { title, text, difficulty, duration, linkTo } = this.props;
     return (
-      <li className={`glide__slide ${styles['SliderItem']}`}>
+      <li className={`glide__slide ${styles['SliderItem']}`} style={{width: 200}}>
         <Link to={linkTo}>
           <div style={{padding: 16}}>
             <div>{title}</div>
@@ -83,47 +83,52 @@ class ShowAllLink extends Component {
 }
 
 class HomePage extends Component {
+
+  navigate = (to) => {
+    this.props.history.push(to);
+  };
+
   render() {
     return (
       <div className={styles['HomePage']}>
-        <section className={styles['RunningSection']}>
+        <section className={styles['RunningSection']} onClick={this.navigate.bind(null, '/runwalk')}>
           <div>
             <div className={styles['SectionTitle']}>Running / Walking</div>
             <div className={styles['SectionDescription']}>Routes for walking</div>
-            <Slider sliderId={1}>
+            {/*<Slider sliderId={1}>
               { RunWalkData.map((track, i) => i > 12 ? null : <SliderItem key={i} title={track.name} text="" difficulty="1" duration="1" linkTo={`/runwalk/track/${track.id}`} />) }
             </Slider>
-            <ShowAllLink to="/runwalk" />
+            <ShowAllLink to="/runwalk" />*/}
           </div>
         </section>
-        <section className={styles['BodyweightSection']}>
+        <section className={styles['BodyweightSection']} onClick={this.navigate.bind(null, '/bodyweight')}>
           <div>
             <div className={styles['SectionTitle']}>Bodyweight</div>
             <div className={styles['SectionDescription']}>Powerful Workouts</div>
-            <Slider sliderId={2}>
+            {/*<Slider sliderId={2}>
               { BodyweightData.exercises.map((exercise, i) => i > 12 ? null : <SliderItem key={i} title={exercise.name} text="" difficulty={exercise.difficulty} duration="1" linkTo={`/bodyweight/exercises/${exercise.id}`} />) }
             </Slider>
-            <ShowAllLink to="/bodyweight" />
+            <ShowAllLink to="/bodyweight" />*/}
           </div>
         </section>
-        <section className={styles['GymSection']}>
+        <section className={styles['GymSection']} onClick={this.navigate.bind(null, '/gym')}>
           <div>
             <div className={styles['SectionTitle']}>Gym</div>
             <div className={styles['SectionDescription']}>Workouts with equipment</div>
-            <Slider sliderId={3}>
+            {/*<Slider sliderId={3}>
               { [...Array(13).keys()].map(i => <SliderItem key={i} title={`Gym Exercise ${i}`} text="This is a placeholder" difficulty="3" duration="1" />) }
             </Slider>
-            <ShowAllLink to="/gym" />
+            <ShowAllLink to="/gym" />*/}
           </div>
         </section>
-        <section className={styles['GroupFitnessSection']}>
+        <section className={styles['GroupFitnessSection']} onClick={this.navigate.bind(null, '/groupfitness')}>
           <div>
             <div className={styles['SectionTitle']}>Group Fitness</div>
             <div className={styles['SectionDescription']}>Community powered Workouts</div>
-            <Slider sliderId={4}>
+            {/*<Slider sliderId={4}>
               { [...Array(13).keys()].map(i => <SliderItem key={i} title={`Group Exercise ${i}`} text="This is a placeholder" difficulty="3" duration="1" />) }
             </Slider>
-            <ShowAllLink to="/groupfitness" />
+            <ShowAllLink to="/groupfitness" />*/}
           </div>
         </section>
         {
